2bd1dc4e7593f84bf0c572ffac39e77d74aac7a9
Anthony Cannon <anthony.cannon@ba.com>
2020-05-22 13:34:50 +0100

    Created basic working project

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-s
+require('dotenv').config();
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
