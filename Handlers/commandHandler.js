f6a33bc5f4d510a6f6d06de10d0238d465c2a52d
Anthony Cannon <anthony.cannon@ba.com>
2020-05-24 11:56:28 +0100

    Bettered command logic

--- Handlers/commandHandler.js
+++ Handlers/commandHandler.js
@@ -1,65 +1,128 @@
 /* eslint-disable no-console */
 /* eslint-disable no-use-before-define */
+
+const isDate = /^[0-9]{2}[\/][0-9]{2}[\/][0-9]{4}$/g;
+const isNumber = /\d+/g;
+
+// /getthreshold office1
 const handleGetThresholdRequest = async ({ command, ack, say }) => {
-  await ack();
-  console.log(`Got command: getthreshold ${command.text}`);
-  if (command.text.length > 0) {
+  var params = splitText(command.text);
+  if (params.length == 0) {
+    await ack(`Ops, looks like you forgot to enter the office name!`);
+  } else {
+    await ack()
     const threshold = getThreshold(command.text);
-    say(`Threshold for ${command.text} is ${threshold}`);
-  } else {
-    say('Remember to write the office your refering to.');
+    say(`Threshold for ${params[0]} is ${threshold}`);
   }
 };
 
+// /whosattending office1 today,tomorrow,dd/MM/yy
 const handleGetWhoIsAttendingRequest = async ({ command, ack, say }) => {
-  await ack();
-  console.log(`Got command: whosattending ${command.text}`);
-  if (command.text.includes(' ')) {
-    const params = command.text.split(' ');
-    const attendees = getAttendees(params[0], params[1]);
-    say(`List of attendees: ${attendees}`);
+  var params = splitText(command.text);
+  if (params.length == 0) {
+    await ack(`Ops, looks like you forgot to enter the office name and date!`);
+
+  } else if (params.length == 1) {
+    await ack(`Ops, looks like you forgot to enter the date!`);
+
   } else {
-    say('Remember to write the office your refering to and the date.');
+    if (params[1] === `today`) {
+      params[1] = getTodaysDate();
+    } else if (params[1] === `tomorrow`) {
+      params[1] = getTomorrowsDate();
+    }
+
+    if (params[1].match(isDate)) {
+      await ack()
+      const attendees = getAttendees(params[0], params[1]);
+      say(`List of attendees: ${attendees}`);
+
+    } else {
+      await ack(`Ops, make sure you write correctly: dd/mm/yyyy`);
+    }
   }
 };
 
+// /removeme office1 today,tomorrow,dd/MM/yy
 const handleRemoveMeRequest = async ({ command, ack, say }) => {
-  await ack();
-  console.log(`Got command: removeme ${command.text}`);
-  if (command.text.includes(' ')) {
-    const params = command.text.split(' ');
-    removeAttendee(command.user_name, params[0], params[1]);
-    say(
-      `${command.user_id
-      } removed from the ${
-        params[0]
-      } attendees list, ${
-        params[1]}`,
-    );
+  var params = splitText(command.text);
+  if (params.length == 0) {
+    await ack(`Ops, looks like you forgot to enter the office name and date!`);
+
+  } else if (params.length == 1) {
+    await ack(`Ops, looks like you forgot to enter the date!`);
+
   } else {
-    say('Remember to write the office your refering to and the date.');
+    if (params[1] === `today`) {
+      params[1] = getTodaysDate();
+    } else if (params[1] === `tomorrow`) {
+      params[1] = getTomorrowsDate();
+    }
+
+    if (params[1].match(isDate)) {
+      await ack()
+      removeAttendee(command.user_name, params[0], params[1]);
+      say(`${command.user_id} removed from the ${params[0]} attendees list`);
+
+    } else {
+      await ack(`Ops, make sure you write correctly: dd/mm/yyyy`);
+    }
   }
 };
 
+// /setthreshold office1 12
 const handleSetThresholdRequest = async ({ command, ack, say }) => {
-  await ack();
-  console.log(`Got command: setthreshold ${command.text}`);
-  if (command.text.includes(' ')) {
-    const params = command.text.split(' ');
+  var params = splitText(command.text);
+  if (params.length == 0) {
+    await ack(`Ops, looks like you forgot to enter the office name and the new theshold!`);
+
+  } else if (params.length == 1) {
+    await ack(`Ops, looks like you forgot to enter the new theshold!`);
+
+  } else if (params[1].match(isNumber)) {
+    await ack()
     setThreshold(params[0], params[1]);
     say(`Threshold is updated for ${params[0]}`);
+
   } else {
-    say('Remember to write the office your refering to and the new threshold.');
+    await ack(`Ops, make sure your new theshold is a number!`);
   }
 };
 
+// Utils
+function splitText(text) {
+  return text.split(` `).filter(function (el) {
+    return el.length > 0;
+  });
+}
+
+function getTodaysDate() {
+  return dateToString(new Date());
+}
+
+function getTomorrowsDate() {
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  return dateToString(tomorrow)
+}
+
+function dateToString(date) {
+  var dd = String(date.getDate()).padStart(2, '0');
+  var mm = String(date.getMonth() + 1).padStart(2, '0');
+  var yyyy = date.getFullYear();
+
+  return dd + '/' + mm + '/' + yyyy;
+}
+
+// DB
 function getThreshold() {
   return 0;
 }
 
-function getAttendees(office, date) {}
+function getAttendees(office, date) { }
 
-function removeAttendee(userId, office, date) {}
+function removeAttendee(userId, office, date) { }
 
 function setThreshold(office, threshold) {
   console.log(`Setting threshold for ${office} to ${threshold}`);
