9c803f1c044a6443b2d1c0c7b22216461600f0f1
Bolu Olubajo <bolu.olubajo@and.digital>
2020-05-26 13:03:31 +0100

    Bug fix relating to lowercase or uppercase inputs

--- Handlers/commandHandler.js
+++ Handlers/commandHandler.js
@@ -16,7 +16,6 @@ officesThreshold.set(ada, 0);
 officesThreshold.set(turing, 0);
 officesThreshold.set(dekker, 0);
 
-
 // /getthreshold office1
 const handleGetThresholdRequest = async ({ command, ack, say }) => {
   const params = splitText(command.text);
@@ -28,7 +27,7 @@ const handleGetThresholdRequest = async ({ command, ack, say }) => {
   } else if (officeExists) {
     await ack();
     const threshold = getThreshold(office);
-    say(`Threshold for ${params[0]} is ${threshold}`);
+    say(`Threshold for ${params[0].toUpperCase()} is ${threshold}`);
   } else {
     await ack('Please enter a valid office name');
   }
@@ -41,7 +40,9 @@ const handleGetWhoIsAttendingRequest = async ({ command, ack, say }) => {
   const day = params[1];
   const officeExists = checkOffice(office);
   if (params.length === 0) {
-    await ack('Ops, looks like you forgot to enter the correct office name and date!');
+    await ack(
+      'Ops, looks like you forgot to enter the correct office name and date!',
+    );
   } else if (params.length === 1) {
     await ack('Ops, looks like you forgot to enter the date!');
   } else if (day.toLowerCase() === 'today' && officeExists) {
@@ -49,18 +50,22 @@ const handleGetWhoIsAttendingRequest = async ({ command, ack, say }) => {
     await ack();
     const attendees = getAttendeesToday(office, params[1]);
     if (attendees) {
-      say(`${attendees} woud be attending ${office} today`);
+      say(`${attendees} woud be attending ${office.toUpperCase()} today`);
     } else {
-      say(`No one at the moment would be attending ${office} today`);
+      say(
+        `No one at the moment would be attending ${office.toUpperCase()} today`,
+      );
     }
   } else if (day.toLowerCase() === 'tomorrow' && officeExists) {
     params[1] = getTomorrowsDate();
     await ack();
     const attendees = getAttendeesTomorrow(office, params[1]);
     if (attendees) {
-      say(`${attendees} woud be attending ${office} tomorrow`);
+      say(`${attendees} woud be attending ${office.toUpperCase()} tomorrow`);
     } else {
-      say(`No one at the moment would be attending ${office} tomorrow`);
+      say(
+        `No one at the moment would be attending ${office.toUpperCase()} tomorrow`,
+      );
     }
   } else {
     await ack('Ops, make sure you write correctly: dd/mm/yyyy');
@@ -74,7 +79,9 @@ const handleRemoveMeRequest = async ({ command, ack, say }) => {
   const day = params[1];
   const officeExists = checkOffice(office);
   if (params.length === 0) {
-    await ack('Ops, looks like you forgot to enter the correct office name and date!');
+    await ack(
+      'Ops, looks like you forgot to enter the correct office name and date!',
+    );
   } else if (params.length === 1) {
     await ack('Ops, looks like you forgot to enter the date!');
   } else if (day.toLowerCase() === 'today' && officeExists) {
@@ -82,24 +89,39 @@ const handleRemoveMeRequest = async ({ command, ack, say }) => {
     await ack();
     const removed = removeAttendeeToday(command.user_id, office, params[1]);
     if (removed) {
-      say(`${command.user_name} has been removed from the ${office} attendees list today`);
+      say(
+        `${
+          command.user_name
+        } has been removed from the ${office.toUpperCase()} attendees list today`,
+      );
     } else {
-      say(`${command.user_name} was not on the list to attend ${office} today`);
+      say(
+        `${
+          command.user_name
+        } was not on the list to attend ${office.toUpperCase()} today`,
+      );
     }
   } else if (day.toLowerCase() === 'tomorrow' && officeExists) {
     params[1] = getTomorrowsDate();
     await ack();
     const removed = removeAttendeeTomorrow(command.user_id, office, params[1]);
     if (removed) {
-      say(`${command.user_name} has been removed from the the ${office} attendees list tomorrow`);
+      say(
+        `${
+          command.user_name
+        } has been removed from the the ${office.toUpperCase()} attendees list tomorrow`,
+      );
     } else {
-      say(`${command.user_name} was not on the list to attend ${office} tomorrow`);
+      say(
+        `${
+          command.user_name
+        } was not on the list to attend ${office.toUpperCase()} tomorrow`,
+      );
     }
   } else {
     say('Ops, make sure you write correctly: dd/mm/yyyy');
   }
 };
-
 
 // /addme office1 today
 const handleAddMeRequest = async ({ command, ack, say }) => {
@@ -108,7 +130,9 @@ const handleAddMeRequest = async ({ command, ack, say }) => {
   const day = params[1];
   const officeExists = checkOffice(office);
   if (params.length === 0) {
-    await ack('Ops, looks like you forgot to enter the correct office name and date!');
+    await ack(
+      'Ops, looks like you forgot to enter the correct office name and date!',
+    );
   } else if (params.length === 1) {
     await ack('Ops, looks like you forgot to enter the date!');
   } else if (day.toLowerCase() === 'today' && officeExists) {
@@ -116,18 +140,36 @@ const handleAddMeRequest = async ({ command, ack, say }) => {
     await ack();
     const added = addAttendeeToday(command.user_id, office, params[1]);
     if (added) {
-      say(`${command.user_name} added to the ${office} attendees list today`);
+      say(
+        `${
+          command.user_name
+        } added to the ${office.toUpperCase()} attendees list today`,
+      );
     } else {
-      say(`${command.user_name} is already on the list to attend ${usersAttendingToday.get(command.user_id)} today`);
+      say(
+        `${
+          command.user_name
+        } is already on the list to attend ${(usersAttendingToday.get(command.user_id)).toUpperCase()} today`,
+      );
     }
   } else if (day.toLowerCase() === 'tomorrow' && officeExists) {
     params[1] = getTomorrowsDate();
     await ack();
     const added = addAttendeeTomorrow(command.user_id, office, params[1]);
     if (added) {
-      say(`${command.user_name} added to the the ${office} attendees list tomorrow`);
+      say(
+        `${
+          command.user_name
+        } added to the the ${office.toUpperCase()} attendees list tomorrow`,
+      );
     } else {
-      say(`${command.user_name} is already on the list to attend ${usersAttendingTomorrow.get(command.user_id)} tomorrow`);
+      say(
+        `${
+          command.user_name
+        } is already on the list to attend ${(usersAttendingTomorrow
+          .get(command.user_id))
+          .toUpperCase()} tomorrow`,
+      );
     }
   } else {
     await ack('Ops, make sure you write correctly: dd/mm/yyyy');
@@ -148,9 +190,11 @@ const handleSetThresholdRequest = async ({ command, ack, say }) => {
   } else if (params[1].match(isNumber) && officeExists) {
     await ack();
     setThreshold(office, params[1]);
-    say(`Threshold is updated for ${params[0]}`);
+    say(`Threshold is updated for ${office.toUpperCase()}`);
   } else {
-    await ack('Ops, make sure your new theshold is a number and the office exists!');
+    await ack(
+      'Ops, make sure your new theshold is a number and the office exists!',
+    );
   }
 };
 
@@ -180,18 +224,17 @@ function dateToString(date) {
 
 // DB
 function getThreshold(office) {
-  const result = officesThreshold.get(office);
+  const result = officesThreshold.get(office.toLowerCase());
   console.log(result);
   return result;
 }
 
-function getAttendeesToday(officeInput, date) {
-  const office = officeInput.toLowerCase();
+function getAttendeesToday(office, date) {
   const ids = Array.from(slackIds);
   const attendees = [];
   let result = '';
   ids.forEach((id) => {
-    if (usersAttendingToday.get(id) === office) attendees.push(id);
+    if (usersAttendingToday.get(id) === office.toLowerCase()) attendees.push(id);
   });
   attendees.forEach((attendee) => {
     result += `<@${attendee}>,`;
@@ -199,13 +242,12 @@ function getAttendeesToday(officeInput, date) {
   return result;
 }
 
-function getAttendeesTomorrow(officeInput, date) {
-  const office = officeInput.toLowerCase();
+function getAttendeesTomorrow(office, date) {
   const ids = Array.from(slackIds);
   const attendees = [];
   let result = '';
   ids.forEach((id) => {
-    if (usersAttendingTomorrow.get(id) === office) attendees.push(id);
+    if (usersAttendingTomorrow.get(id) === office.toLowerCase()) attendees.push(id);
   });
   attendees.forEach((attendee) => {
     result += `<@${attendee}>,`;
@@ -233,7 +275,7 @@ function addAttendeeToday(userId, office, date) {
   if (!usersAttendingToday.has(userId)) {
     console.log(`Adding ${userId} to attend ${office} on ${date}`);
     slackIds.add(userId);
-    return usersAttendingToday.set(userId, office);
+    return usersAttendingToday.set(userId, office.toLowerCase());
   }
   return false;
 }
@@ -241,14 +283,14 @@ function addAttendeeTomorrow(userId, office, date) {
   if (!usersAttendingTomorrow.has(userId)) {
     console.log(`Adding ${userId} to attend ${office} on ${date}`);
     slackIds.add(userId);
-    return usersAttendingTomorrow.set(userId, office);
+    return usersAttendingTomorrow.set(userId, office.toLowerCase());
   }
   return false;
 }
 
 function setThreshold(office, threshold) {
   console.log(`Setting threshold for ${office} to ${threshold}`);
-  officesThreshold.set(office, threshold);
+  officesThreshold.set(office.toLowerCase(), threshold);
 }
 
 function checkOffice(input) {
